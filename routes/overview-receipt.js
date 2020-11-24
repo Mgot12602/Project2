ea7982523b1cc7fe82a33da6c10fcd74033a67b8
Marc Gotzens <marcgotzens@hotmail.com>
2020-11-24 14:12:21 +0100

    started with the sort in populate

--- routes/overview-receipt.js
+++ routes/overview-receipt.js
@@ -5,8 +5,6 @@ const isLoggedIn = require("../middlewares/isLoggedIn");
 
 const router = require("express").Router();
 
-<<<<<<< HEAD
-=======
 // * Find receipt with porperty called user with value = user._id
 //router.get("/", isLoggedIn, (req, res) => {
 //  Receipt.find(user.req.session.user._id) // only my receipts
@@ -21,11 +19,9 @@ const router = require("express").Router();
 
 // Find the user and show his receipts
 // populate the array of receipts
->>>>>>> 56424145e107ce2fabbc0bd5e60d6245d3a46af8
 router.get("/", isLoggedIn, (req, res) => {
   User.find(req.session.user._id) // only my receipts
-    .populate({ path: "receipts" })
-    .sort({ category: 1 })
+    .populate({ path: "receipts", options: { sort: "date" } })
     .then((populatedUser) => {
       console.log("What we get after populating:", populatedUser);
       const receipts = populatedUser[0].receipts;
