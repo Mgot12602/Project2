9b87c72b9c81a83c4e5b418e13a1f2cd1cd4e6d9
Marc Gotzens <marcgotzens@hotmail.com>
2020-11-24 12:45:47 +0100

    merging with marc

--- routes/overview-receipt.js
+++ routes/overview-receipt.js
@@ -5,23 +5,10 @@ const isLoggedIn = require("../middlewares/isLoggedIn");
 
 const router = require("express").Router();
 
-// * Find receipt with value = user._id of porperty User
-//router.get("/", isLoggedIn, (req, res) => {
-//  Receipt.find(req.session.user._id) // only my receipts
-//    .sort({ date: -1 })
-//    //.limit(10)
-//    //.populate("")
-//    .then((receipts) => {
-//      console.log("receipts:", receipts);
-//      res.render("overview-receipts", { receipts });
-//    });
-//});
-
-// instead of finding the receipt. Find the user and show his receipts
-// populate the array of receipts
 router.get("/", isLoggedIn, (req, res) => {
   User.find(req.session.user._id) // only my receipts
-    .populate("receipts")
+    .populate({ path: "receipts" })
+    .sort({ category: 1 })
     .then((populatedUser) => {
       console.log("What we get after populating:", populatedUser);
       const receipts = populatedUser[0].receipts;
